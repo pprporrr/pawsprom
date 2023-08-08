aec933bbb1c994bc8a1fffb4a9ae43f4c9b30d27
ppr <pprporrr@gmail.com>
2023-08-09 02:43:19 +0700

    add 2 new endpoint

--- backend/APIs/petAPIs.py
+++ backend/APIs/petAPIs.py
@@ -53,6 +53,44 @@ async def create_pet(request: Request):
     finally:
         await db_connector.disconnect()
 
+@router.get("/pet/", response_model=dict)
+async def read_pet_details(request: Request):
+    try:
+        await db_connector.connect()
+        data = await request.json()
+        
+        petID = data.get("petID")
+        
+        if petID is None:
+            return create_error_response("missing 'petID' in the request data")
+        
+        query = "SELECT * FROM pet WHERE petID = %s"
+        result = await db_connector.execute_query(query, petID)
+        
+        if not result:
+            return create_error_response("pet not found")
+        
+        petDetails = {
+            "petID": result[0][0],
+            "petName": result[0][1],
+            "species": result[0][2],
+            "breed": result[0][3],
+            "age": result[0][4],
+            "gender": result[0][5],
+            "weight": result[0][6],
+            "color": result[0][7],
+            "dateofbirth": result[0][8].isoformat(),
+            "description": result[0][9],
+            "availabilityStatus": result[0][10],
+            "shelters_shelterID": result[0][11]
+        }
+        
+        return create_success_response(petDetails)
+    except Exception as e:
+        return create_error_response(str(e))
+    finally:
+        await db_connector.disconnect()
+
 @router.get("/pet/list/", response_model=dict)
 async def read_pet_details_list(request: Request):
     try:
@@ -101,7 +139,7 @@ async def read_pet_details_list(request: Request):
         await db_connector.disconnect()
 
 @router.get("/pet/list_in_shelter/", response_model=dict)
-async def read_pet_details_list(request: Request):
+async def read_pet_details_list_in_shelter(request: Request):
     try:
         await db_connector.connect()
         data = await request.json()
@@ -233,6 +271,114 @@ async def delete_pet(request: Request):
                 await cursor.execute(query, petID)
         
         return create_success_response("pet deleted")
+    except Exception as e:
+        return create_error_response(str(e))
+    finally:
+        await db_connector.disconnect()
+
+@router.get("/pet/infoshow/", response_model=dict)
+async def read_pet_details(request: Request):
+    try:
+        await db_connector.connect()
+        data = await request.json()
+        
+        petID = data.get("petID")
+        
+        if petID is None:
+            return create_error_response("missing 'petID' in the request data")
+        
+        petDetailsquery = "SELECT * FROM pet WHERE petID = %s"
+        petDetailsresult = await db_connector.execute_query(petDetailsquery, petID)
+        
+        petImagesquery = "SELECT * FROM petImages WHERE pet_petID = %s"
+        petImagesresult = await db_connector.execute_query(petImagesquery, petID)
+        
+        if not petDetailsresult:
+            return create_error_response("pet not found")
+        
+        if petDetailsresult[0][10] == "Available":
+            petAddressquery = "SELECT * FROM shelter WHERE shelterID = %s"
+            petAddressresult = await db_connector.execute_query(petAddressquery, petDetailsresult[0][11])
+            
+            petInfo = {
+                "petName": petDetailsresult[0][1],
+                "species": petDetailsresult[0][2],
+                "breed": petDetailsresult[0][3],
+                "address": petAddressresult[0][2],
+                "availabilityStatus": petDetailsresult[0][10],
+                "imageURLs": [row[2] for row in petImagesresult]
+            }
+        elif petDetailsresult[0][10] == "Adopted" or petDetailsresult[0][10] == "Owned":
+            petOwnerquery = "SELECT * FROM petOwnership WHERE pet_petID = %s"
+            petOwnerresult = await db_connector.execute_query(petOwnerquery, petID)
+            
+            ownerAddressquery = "SELECT * FROM user WHERE userID = %s"
+            ownerAddressresult = await db_connector.execute_query(ownerAddressquery, petOwnerresult[0][2])
+            
+            petInfo = {
+                "petName": petDetailsresult[0][1],
+                "species": petDetailsresult[0][2],
+                "breed": petDetailsresult[0][3],
+                "address": ownerAddressresult[0][6],
+                "availabilityStatus": petDetailsresult[0][10],
+                "imageURLs": [row[2] for row in petImagesresult]
+            }
+        else:
+            petInfo = {
+                "petName": petDetailsresult[0][1],
+                "species": petDetailsresult[0][2],
+                "breed": petDetailsresult[0][3],
+                "availabilityStatus": petDetailsresult[0][10],
+                "imageURLs": [row[2] for row in petImagesresult]
+            }
+        
+        return create_success_response(petInfo)
+    except Exception as e:
+        return create_error_response(str(e))
+    finally:
+        await db_connector.disconnect()
+
+@router.get("/pet/info/", response_model=dict)
+async def read_pet_details(request: Request):
+    try:
+        await db_connector.connect()
+        data = await request.json()
+        
+        petID = data.get("petID")
+        
+        if petID is None:
+            return create_error_response("missing 'petID' in the request data")
+        
+        petDetailsquery = "SELECT * FROM pet WHERE petID = %s"
+        petDetailsresult = await db_connector.execute_query(petDetailsquery, petID)
+        
+        petImagesquery = "SELECT * FROM petImages WHERE pet_petID = %s"
+        petImagesresult = await db_connector.execute_query(petImagesquery, petID)
+        
+        petVaccinesquery = "SELECT * FROM petVaccinations WHERE pet_petID = %s"
+        petVaccinesresult = await db_connector.execute_query(petVaccinesquery, petID)
+        
+        if not petDetailsresult:
+            return create_error_response("pet not found")
+        
+        petInfo = {
+            "petName": petDetailsresult[0][1],
+            "species": petDetailsresult[0][2],
+            "breed": petDetailsresult[0][3],
+            "age": petDetailsresult[0][4],
+            "gender": petDetailsresult[0][5],
+            "weight": petDetailsresult[0][6],
+            "color": petDetailsresult[0][7],
+            "dateofbirth": petDetailsresult[0][8].isoformat(),
+            "description": petDetailsresult[0][9],
+            "availabilityStatus": petDetailsresult[0][10],
+            "shelterID": petDetailsresult[0][11],
+            "imageURLs": [row[2] for row in petImagesresult],
+            "vaccinationName": [row[2] for row in petVaccinesresult],
+            "vaccinationDate": [row[3] for row in petVaccinesresult]
+        }
+        
+        return create_success_response(petInfo)
     except Exception as e:
         return create_error_response(str(e))
     finally:
