0d54558559e590315277315b245491a07a4b8f0f
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-09-12 13:36:27 +0700

    login store ID in localStorage/Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
+import styles from './App.module.css'
 import { Routes, Route } from "react-router-dom";
+import { NavBar } from './Components/NavBar/NavBar.tsx';
 import { PetProfileOwned } from './Pages/PetProfileOwned/petProfileOwned.tsx';
 import { CreatePetProfile } from './Pages/CreatePetProfile/createPetProfile.tsx';
+import { LoginPage } from "./Pages/Login/LoginPage.tsx";
 import { Home } from './Pages/Home/home.tsx';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/p" element={<PetProfileOwned />} />
-      <Route path="/create" element={<CreatePetProfile />} />
+    <div className={styles.main_container}>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/p" element={<PetProfileOwned />} />
+        <Route path="/create" element={<CreatePetProfile />} />
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
-
+      <div className={styles.footer }>
+        Copyright © [Year of Creation] Cloud Go Go Pet Adoption. All rights reserved.
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/PetProfileOwned/petProfileOwned.tsx
+++ frontend/src/Pages/PetProfileOwned/petProfileOwned.tsx
@@ -103,26 +103,22 @@ export const PetProfileOwned = () => {
     }, [])
      // *---------------- POST to API ----------------* //
 
-    return (
-      <div className={styles.main_container}>
-        <section className={styles.header}>header</section>
-          <div className={styles.bgContainer}>
-            { data != undefined && <PetProfileFull
-            petID={petID} 
-            page={petPage}
-            data= {data}
-            baseAPI={baseAPI} 
-            ></PetProfileFull>
-            }
-            {/* //! for testing */}
-            <PetProfileFull
-            petID={petID} 
-            page={petPage}
-            data= {Data}
-            baseAPI={baseAPI} 
-            ></PetProfileFull>
-          </div>
-        <section className={styles.bottom}>bottom</section>
-      </div>
+  return (
+    <div className={styles.bgContainer}>
+      { data != undefined && <PetProfileFull
+      petID={petID} 
+      page={petPage}
+      data= {data}
+      baseAPI={baseAPI} 
+      ></PetProfileFull>
+      }
+      {/* //! for testing */}
+      <PetProfileFull
+      petID={petID} 
+      page={petPage}
+      data= {Data}
+      baseAPI={baseAPI} 
+      ></PetProfileFull>
+    </div>
     )
 }
